e0aebab36def4ecec06537aaf3e9eb5468550879
Gareth Devlin <33987109+GLD5000@users.noreply.github.com>
2022-10-04 18:36:56 +0000

    Updated state object

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,9 +6,9 @@ import Tasks from "./components/Tasks";
 
 function App() {
   const [taskList, setTask] = useState([
-    { id: 1, text: "hello", day: "4th Feb 2022", reminder: true },
-    { id: 2, text: "eargerh", day: "44th Feb 2022", reminder: true },
-    { id: 3, text: "thrserthr", day: "444th Feb 2022", reminder: true },
+    { id: 1, day: "4th Feb 2022", tags: ["JavaScript", "Pitfall"], title: "Title", code:"codey-codey = Code", text: "hello", table: [["Row1", "col1 Row 2"], ["Row2", "col2 row2"]] },
+    { id: 2, day: "44th Feb 2022", tags: ["JavaScript", "Pitfall"], title: "Title", code:"codey-codey = Code", text: "eargerh", table: [["Row1", "col1 Row 2"], ["Row2", "col2 row2"]] },
+    { id: 3, day: "444th Feb 2022", tags: ["JavaScript", "Pitfall"], title: "Title", code:"codey-codey = Code", text: "thrserthr", table: [["Row1", "col1 Row 2"], ["Row2", "col2 row2"]] },
   ]);
   
   return (
