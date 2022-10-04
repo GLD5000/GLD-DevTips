e840b78a2b60ea31356a5c2e61a11356b4c6bb10
Gareth Devlin <33987109+GLD5000@users.noreply.github.com>
2022-10-04 09:08:40 +0000

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  const name = "Gfunk"
+  return (
+    <container className="container">
+      <h1>Hello from {name}</h1>
+      <h1>Hi there</h1>
+    </container>
+  );
+}
+
+export default App;
