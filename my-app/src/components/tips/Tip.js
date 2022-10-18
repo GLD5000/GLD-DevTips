d05c5038702bbb8d1d454167a2ad4bb586402c28
GLD5000 <garethlouisdevlin@gmail.com>
2022-10-18 12:24:32 +0100

    Added tags and removed logos from manifest.json

--- my-app/src/components/tips/Tip.js
+++ my-app/src/components/tips/Tip.js
@@ -1,16 +1,17 @@
 import MultiTextBox from "./MultiTextBox";
 import TipTitle from "../../elements/TipTitle";
+import Tags from "./Tags";
 
 const Tip = ({ tip }) => {
-  return (
-    <div className="tip">
-        <TipTitle title={tip.title} />
-          {tip.sections.map((object, index) => (
-        <MultiTextBox key={index} object={object} />
-      ))}
-
-    </div>
-  );
+	return (
+		<div className="tip">
+				<TipTitle title={tip.title} />
+				{tip.sections.map((object, index) => (
+					<MultiTextBox key={index} object={object} />
+				))}
+				<Tags tagArray={tip.tags}/>
+		</div>
+	);
 };
 
 export default Tip;
