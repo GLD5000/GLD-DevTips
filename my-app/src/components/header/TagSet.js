b4534b2e6919e1c3e0bf3c310d602fe2339fc33e
Gareth Devlin <33987109+GLD5000@users.noreply.github.com>
2022-10-19 20:23:28 +0000

    Updated Filter Styling

--- my-app/src/components/header/TagSet.js
+++ my-app/src/components/header/TagSet.js
@@ -4,13 +4,13 @@ const TagSet = ({ tagSet }) => {
 
   const buttonArray = [];
   return (
-    <>
+    <section className="filter-container">
       {tagSet.forEach((tag, key) =>
         buttonArray.push(<TagFilter key={key} tag={tag} />)
       )}
       ;
       {buttonArray}
-    </>
+    </section>
   );
 };
 
