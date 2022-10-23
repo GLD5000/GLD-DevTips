d85d319a15789a7f36a10297f3a86f91bba7ce21
GLD5000 <garethlouisdevlin@gmail.com>
2022-10-23 12:59:33 +0100

    Pseudocoded markdownParser

---
+++ my-app/src/utilities/markdownParser.js
@@ -0,0 +1,9 @@
+export default function markdownParser() {
+
+    //loop through string build array of types
+    //split string using regex encompassing all flags
+  return (
+    `Array with pairs of entries e.g.:
+    [[type][content], [type],[content]]`
+  )
+}
