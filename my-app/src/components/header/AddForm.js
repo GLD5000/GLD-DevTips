78811f9a1631129a80be86a06bf4716ffa409844
GLD5000 <garethlouisdevlin@gmail.com>
2022-10-27 17:49:22 +0100

    Started parser in Add form

--- my-app/src/components/header/AddForm.js
+++ my-app/src/components/header/AddForm.js
@@ -1,14 +1,29 @@
 import { useState } from "react"
 import MultiInput from "./MultiInput"
+function parseNewTip(){
+  //add title to state
+  //Parse new tip from state
+  const modelObject =     {
+    id: "", //string
+    date: "4th Feb 2022",// string
+    tags: ["JavaScript", "How-To"],// array of strings
+    title: "Crockford Objects",// string
+    sections: [ // array of objects
+      {
+        title: "",//string
+        type: "",//string
+        content: "",//string
+      },
+    ],
+  };
+  console.log(modelObject)
+
+}
 
 const AddForm = ({setTip}) => {
   const [inputState, setInputState] = useState(() => { return {0:
     { type: "text", content: null },
   }});
-  function parseNewTip(){
-    //add title to state
-    //Parse new tip from state
-  }
   return (
     <div className="add-form">
           <div className="form-control">
@@ -19,4 +34,5 @@ const AddForm = ({setTip}) => {
         </div>
   )
 }
+parseNewTip();
 export default AddForm
