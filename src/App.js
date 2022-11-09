84d1783ccee5270015d42e9949a922aaf29ad2f3
Gareth Devlin <33987109+GLD5000@users.noreply.github.com>
2022-11-09 20:55:34 +0000

    Removed line comment from import firebase lines

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/header/Header";
 import Tips from "./components/tips/Tips";
 
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
