aaeb249e780def71328ed80856d97a09a834c242
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-09 20:39:51 +0000

    Began adding Firestore and emulator for Firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import { useState } from "react";
 import Header from "./components/header/Header";
 import Tips from "./components/tips/Tips";
 
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ7I6lUNnmKkJd60Gyoox-QfzO5wKdjCU",
+  authDomain: "devtips-c1b63.firebaseapp.com",
+  projectId: "devtips-c1b63",
+  storageBucket: "devtips-c1b63.appspot.com",
+  messagingSenderId: "616417597533",
+  appId: "1:616417597533:web:445aabc894ece7a57495b4",
+  measurementId: "G-D9GBBQW1S0"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   const [tipList, setTip] = useState([
     {
