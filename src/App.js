ef0edd6784cbd6ea927c4a365ac0fbfdbf88ef0a
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-10 09:17:36 +0000

    Removed import

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/header/Header";
 import Tips from "./components/tips/Tips";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,8 +22,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.getAnalytics(app);
 
 function App() {
   const [tipList, setTip] = useState([
