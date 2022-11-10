c6351dcc32adf1a02821ff56a0fd599fa77eab31
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-10 13:25:24 +0000

    Added Auth and Firestore imports

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import { useState } from "react";
 import Header from "./components/header/Header";
 import Tips from "./components/tips/Tips";
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBJ7I6lUNnmKkJd60Gyoox-QfzO5wKdjCU",
   authDomain: "devtips-c1b63.firebaseapp.com",
