7878fe73ce44986645838c188fd458ed37894451
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-10 09:14:36 +0000

    Adeed imports back in for firebase

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/header/Header";
 import Tips from "./components/tips/Tips";
 
 // Import the functions you need from the SDKs you need
-//import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,7 +22,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 function App() {
