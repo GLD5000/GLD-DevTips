37e672a85a64ceb2b35d22fa1ca0af6478bb698b
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-11 07:43:22 +0000

    Added db for Firestore

--- src/App.js
+++ src/App.js
@@ -21,12 +21,155 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 //Auth (emulator)
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 function App() {
 
+  const [signedIn, setSignedIn] = useState(() => false);
+  
+  async function  authClickHandler() {
+    if (signedIn) await auth.signOut();
+    if (!signedIn) await signInWithPopup(auth, provider);
+    setSignedIn(() => !signedIn);
+  }
+  const [tipList, setTip] = useState([
+    {
+      id: 1,
+      date: "11th Oct 2022",
+      tags: ["Github", "Bash", "Beginner", "VSCode"],
+      title: "How to clone a Repo from Github",
+      sections: [
+        {
+          type: "code",
+          content: `git clone [Your Repo Url copied from GitHub]`,
+        },
+        {
+          type: "text",
+          content: `##This makes a linked copy of a repo from your Github Account to your computer.
+
+The above command will clone or copy the repo to your default path.`,
+        },
+        {
+          type: "hint",
+          content: `Within VSCode you can change the default path setting in your preferences by searching 'Git Clone' and editing your 'settings.json'`,
+        },
+        {
+          type: "text",
+          content: `#You can open the cloned repo within VSCode by using the open folder option from the 'File' menu.`,
+        },
+        {
+          type: "text",
+          content: `#[Tester T](ext Containing a {link}[**Bold** _**bolditalic**_ of link](https://www.google.co.uk/){link} {link}[Text of link](https://www.google.com/){link}.
+          
+          n. ordered  list item
+          n. ordered list item
+          n. ordered list item
+          - bullet points
+          - bullet points
+          - bullet points`,
+        },
+      ],
+    },
+    {
+      id: 2,
+      date: "4th Feb 2022",
+      tags: ["JavaScript", "How-To"],
+      title: "Crockford Objects",
+      sections: [
+        {
+          title: "Test Title",
+
+          type: "code",
+          content: `function createObject(parameterA, parameterB) {
+  
+  function concatenateValues() {
+    return \`\${parameterA}\${parameterB}\`;
+  }
+      
+  return {
+    parameterA,
+    parameterB,
+    concatenateValues,
+  };
+}
+      
+const object = createObject("A", "B");
+      `,
+        },
+        {
+          type: "hint",
+          content:
+            "You can also add 'Object.freeze' to your return object to make it immutable!",
+        },
+        {
+          type: "code",
+          content: `return Object.freeze({
+  parameterA,
+  parameterB,
+  concatenateValues,
+});`,
+        },
+      ],
+    },
+    {
+      id: 3,
+      date: "4th Feb 2022",
+      tags: ["JavaScript", "Nomenclature"],
+      title: "Parameters Vs Arguments",
+      sections: [
+        {
+          title: "Parameters",
+          type: "text",
+          content: "These are the names for values passed into a function.",
+        },
+        {
+          title: "Arguments",
+          type: "text",
+          content: "These are the actual values passed into a function.",
+        },
+        {
+          type: "table",
+          title: "Test Title",
+
+          content: [
+            ["Parameter", "Argument", "Variable", "Constant"],
+            [
+              "Name / Placeholder for values of a function.",
+              "Actual value given to a function.",
+              "A named reference to a value that can change.",
+              "A value that cannot change.",
+            ],
+          ],
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Pure Functions",
+      date: "4th Feb 2022",
+      tags: ["JavaScript", "Fundamentals"],
+      sections: [
+        {
+          type: "text",
+          content: `Similar to mathematical functions. 
+
+A given input will always return the same output. 
+
+They only return values and do not mutate objects or create side effects. 
+
+This means they are easy to test, can be composed and can be run in parallel without blocking each other.`,
+        },
+      ],
+    },
+  ]);
+  const tagList = Object.fromEntries([
+    ...new Set(tipList.flatMap((tip) => tip.tags).map((x) => [x, "visible"])),
+  ]);
+  const tagListAll = Object.keys(tagList);
+  const [tagState, setTagState] = useState(() => 
   const [signedIn, setSignedIn] = useState(() => false);
   
   async function  authClickHandler() {
