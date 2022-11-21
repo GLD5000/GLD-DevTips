d2a3b6a996791f08257baba0abdd047c57bd59d0
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-21 21:30:21 +0000

    Added AddSvg

---
+++ src/components/icons/Add.js
@@ -0,0 +1,26 @@
+export default function AddSvg() {
+    return (
+     <div className="svg-wrapper">
+     <svg
+       id="add-svg"
+       alt="Add Section"
+       height="100%"
+       width="100%"
+       viewBox="0 0 10 10"
+     >
+      <path d="M 1,5
+      H 9 
+      M 5,1
+      V 9"        
+      style={{
+        stroke: "#ffffff",
+        strokeWidth: "20%",
+        strokeLinecap: "round",
+        fill: "none",
+      }}
+      />
+     </svg>
+   </div>
+  )
+  }
+  
