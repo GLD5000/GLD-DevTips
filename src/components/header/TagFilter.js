c151e33b2f49a055c8b2d8511904d9e1de42d44b
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-22 15:45:06 +0000

    Removed comments

--- src/components/header/TagFilter.js
+++ src/components/header/TagFilter.js
@@ -1,56 +1,45 @@
 import Button from "../../elements/Button";
-import tagHexLookup from "../../utilities/tagHex.js"
+import tagHexLookup from "../../utilities/tagHex.js";
 const TagFilter = ({ tag, setTagState, tagState }) => {
   const isActive = tagState[tag] === "active";
-  function updateTagState(tag, newValue){
+  function updateTagState(tag, newValue) {
     setTagState((object) => {
-      return {...object, [tag]: newValue}; // Tip return new object to trigger re-render
+      return { ...object, [tag]: newValue }; // Tip return new object to trigger re-render
     });
   }
-  // function tagOnPointerOver(e){
-  //   pointer = e.target.id;
-  // };
-
   function tagOnClick(e) {
-    // console.log(pointer);
-    // console.log(e.target.id);
-    // if (pointer !== e.target.id) return;
     const tag = e.target.innerHTML;
     const newValue = isActive ? "visible" : "active";
     updateTagState(tag, newValue);
   }
-  const {backgroundColour, textColour} = tagHexLookup(tag);
+  const { backgroundColour, textColour } = tagHexLookup(tag);
   const className = "btn tag-filter-btn";
-  
 
-  return (<>
-{    isActive ? (
-      <Button
-      name={tag + "filterButton"}
-        id={tag + "filterButton"}
-        color={textColour}
-        backgroundColor={backgroundColour}
-        text={tag}
-        clickFunction={tagOnClick}
-        className={className}
-      />
-
-  ) : (
-      <Button
-      name={tag + "filterButton"}
-
-      id={tag + "filterButton"}
-      color={textColour}
-      backgroundColor={backgroundColour}
-      text={tag}
-      clickFunction={tagOnClick}
-      className={className}
-      />
-
-  )
-}
-  </>
-  )
+  return (
+    <>
+      {isActive ? (
+        <Button
+          name={tag + "filterButton"}
+          id={tag + "filterButton"}
+          color={textColour}
+          backgroundColor={backgroundColour}
+          text={tag}
+          clickFunction={tagOnClick}
+          className={className}
+        />
+      ) : (
+        <Button
+          name={tag + "filterButton"}
+          id={tag + "filterButton"}
+          color={textColour}
+          backgroundColor={backgroundColour}
+          text={tag}
+          clickFunction={tagOnClick}
+          className={className}
+        />
+      )}
+    </>
+  );
 };
 
 export default TagFilter;
