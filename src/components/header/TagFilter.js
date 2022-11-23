9c971eea043e1478117233ff02ea1a798be914ec
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-23 11:08:54 +0000

    Cleaned up tagfilter and togglebutton

--- src/components/header/TagFilter.js
+++ src/components/header/TagFilter.js
@@ -3,23 +3,15 @@ import ToggleButton from "../../elements/ToggleButton";
 import tagHexLookup from "../../utilities/tagHex.js";
 const TagFilter = ({ tag, setTagState, tagState }) => {
   const isActive = tagState[tag] === "active";
- // if (tag === "Github") console.log(`${tag} isActive ${isActive}`);
 
   function updateTagState(tag, newValue) {
     setTagState((object) => {
       return { ...object, [tag]: newValue }; // Tip return new object to trigger re-render
     });
   }
-  // function tagOnPointerOver(e){
-  //   pointer = e.target.id;
-  // };
 
   function tagOnClick(e) {
-    // console.log(pointer);
-    // console.log(e.target.id);
-    // if (pointer !== e.target.id) return;
     const tag = e.target.name;
-    //console.log(`tag ${tag}`);
     
     const newValue = isActive ? "visible" : "active";
     if (tag !== undefined) updateTagState(tag, newValue);
--- src/elements/ToggleButton.js
+++ src/elements/ToggleButton.js
@@ -19,7 +19,6 @@ export default function ToggleButton ({
   className="btn",
   toggle
 }) {
-  //if (name === "Github")console.log(`toggle ${toggle}`);
   
   function getSvg(type){
     const svgLookup = {
@@ -29,13 +28,8 @@ export default function ToggleButton ({
   }
   const svg = getSvg(type)[toggle];
   
-  let pointerTarget;
-  function pointerHandler(e){
-    pointerTarget = e.target.id;
-  }
   function clickHandler(e){
-    //console.log(e.target);
-   // if (pointerTarget !== e.target.id) return;
+    //return if wrong
     clickFunction(e);
   }
 
@@ -46,7 +40,6 @@ export default function ToggleButton ({
       id={id}
       name={name}
       onClick={clickHandler}
-      onPointerOver={pointerHandler}
       className={className}
       style={{ color: color, backgroundColor: backgroundColor, borderRadius: borderRadius }}
     >
