baeb8853cc25918bd5952983621b57b448234bab
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-24 08:40:24 +0000

    Centred Tip Titles

--- src/elements/TipTitle.js
+++ src/elements/TipTitle.js
@@ -3,9 +3,9 @@ import ExpandSvg from "../components/icons/Expand";
 
 const TipTitle = ({ title, onClick, expanded }) => {
   return (
-    <button className="tip-title" onClick={onClick}>
-      <h2>{title}</h2>
-      {expanded ? <CollapseSvg /> : <ExpandSvg />}
+    <button className="tip-title-btn" onClick={onClick}>
+      <h2 className="tip-title-txt">{title}</h2>
+      {expanded ? <CollapseSvg className="tip-title-svg" /> : <ExpandSvg className="tip-title-svg" />}
     </button>
   );
 };
