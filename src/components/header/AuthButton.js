7f9289829ea5078a23ebd9c70bf2b388ad7b8317
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-24 15:54:45 +0000

    Updated header / body alignment

--- src/components/header/AuthButton.js
+++ src/components/header/AuthButton.js
@@ -1,7 +1,8 @@
 import Button from "../../elements/Button"
 export default function AuthButton({authClickHandler, signedIn}) {
   const text = signedIn? "Sign Out": "Sign In With Google";
+  const className = "btn auth-btn"
   return (
-    <Button text={text} clickFunction={authClickHandler}/>
+    <Button text={text} clickFunction={authClickHandler} className={className}/>
   )
 }
