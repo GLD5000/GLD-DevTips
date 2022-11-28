78148f0309d2e5609d433c808b3e2c21c357ae4f
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-28 12:25:46 +0000

    Aded keyMod

--- src/components/header/TagSet.js
+++ src/components/header/TagSet.js
@@ -1,30 +1,34 @@
 import TagFilter from "./TagFilter";
 import Button from "../../elements/Button";
 
-const TagSet = ({ setTagState, tagState , title = "Filter Tags"}) => {
+const TagSet = ({ setTagState, tagState, title = "Filter Tags", keyMod=0 }) => {
 
-    function clearTags(){
-      setTagState((old) => {
-        const newObject = {...old};
-        Object.keys(newObject).forEach((key) => {
-          newObject[key] = "visible";
-        });
+
+  function clearTags() {
+    setTagState((old) => {
+      const newObject = { ...old };
+      Object.keys(newObject).forEach((key) => {
+        newObject[key] = "visible";
+      });
       return newObject;
-      })
-
-    }
-    function tagStateReducer(acc, entry, key) {// make first button a clear all
-      const tag = entry[0];
-      acc.push(<TagFilter
-        key={tag + "A" + key + 1}
+    });
+  }
+  function tagStateReducer(acc, entry, key) {
+    // make first button a clear all
+    const tag = entry[0];
+    acc.push(
+      <TagFilter
+        key={tag + "A" + key + 1 + "B" + keyMod}
         tag={tag}
         setTagState={setTagState}
         tagState={tagState}
-        />);
-      return acc;
-    }
-    function makeButtonArray() {
-      return Object.entries(tagState).reduce(tagStateReducer, [          <Button
+      />
+    );
+    return acc;
+  }
+  function makeButtonArray() {
+    return Object.entries(tagState).reduce(tagStateReducer, [
+      <Button
         name="Clear Tags"
         id="clear-tags"
         key={"clearA0"}
@@ -32,21 +36,19 @@ const TagSet = ({ setTagState, tagState , title = "Filter Tags"}) => {
         backgroundColor={"IndianRed"}
         text={"Clear Tags"}
         clickFunction={clearTags}
-        />
-]);
-    }
-    const buttonArray = makeButtonArray();
-    
-    return (
+      />,
+    ]);
+  }
+  const buttonArray = makeButtonArray();
+
+  return (
     <label>
       <h2>{title}</h2>
       <section className="filter-container">
-        <>
-        {buttonArray}
-        </>
+        <>{buttonArray}</>
       </section>
     </label>
-    );
+  );
 };
 
 export default TagSet;
