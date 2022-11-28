f4942ac577a19c69d7c208afc9c166c1d288cedb
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-28 12:27:39 +0000

    Added get unique tags

--- src/elements/SelectMulti.js
+++ src/elements/SelectMulti.js
@@ -1,36 +1,37 @@
 import InputText from "./InputText";
 import { useState, useEffect } from "react";
 import TagSet from "../components/header/TagSet";
-
+let keyMod = 0
 export default function SelectMulti({
   tagListAll,
   inputFormState,
   addFieldToInputFormState,
 }) {
-  const [newInputTagState, setNewInputTagState] = useState(() => {
-   const newObject = {};
-   tagListAll.sort().forEach((tag) => {
-    newObject[tag] = "visible";
-   });
-   return newObject;
+function initNewInputTagState(){
+  const newObject = {};
+  tagListAll.sort().forEach((tag) => {
+   newObject[tag] = "visible";
   });
-    
+  inputFormState.tags.forEach((tag) => {
+   newObject[tag] = "active";
+  });
+  return newObject;
+}
 
-
+  const [newInputTagState, setNewInputTagState] = useState(initNewInputTagState());
+  
   const inputFormStateTags = inputFormState?.tags || null;
   const inputFormStateTagsString = inputFormStateTags?.join(", ") || null;
   const [customTags, setCustomTags] = useState(
-    () => inputFormState?.tags || []
-  );
+     () =>  []
+    // () => inputFormState?.tags || []
+    );
   function selectedTagReducer(acc, entry){
     if (entry[1] === "active") acc.push(entry[0]);
     return acc;
   }
   function getSelectedTagArray(newInputTagState){
     if (newInputTagState == null) return [];
-    console.group(`newInputTagState`);
-    console.log(newInputTagState);
-    console.groupEnd();
     
     return Object.entries(newInputTagState).reduce(selectedTagReducer, []);
   }
@@ -39,21 +40,39 @@ export default function SelectMulti({
     const selectedTags = getSelectedTagArray(newInputTagState);
     const text = e;
     const tags = text.split(/[,\s]+/).filter((tag) => tag.length > 0);
-
+    const uniqueTags = getUniqueTags(selectedTags, tags);
     setCustomTags(() => {
-      return tags;
+      return uniqueTags;
     });
-    addFieldToInputFormState("tags", [...selectedTags, ...tags]);
+    addFieldToInputFormState("tags", uniqueTags);
   }
 
+  function getUniqueTags(arrayA, arrayB){
+    return [...new Set([...arrayA, ...arrayB])];
 
+  }
+
+  useEffect(()=> {
+    const selectedTags = getSelectedTagArray(newInputTagState);
+    const uniqueTags = [...new Set([...selectedTags, ...customTags.filter(tag => newInputTagState[tag] !== "visible")])];
+    setCustomTags(() => {
+      return [...uniqueTags]; 
+    });
+    keyMod +=1;
+    addFieldToInputFormState("tags", uniqueTags)
+  },[newInputTagState] );
+
+  useEffect(() => {    keyMod +=1;
+  }, [inputFormState])
+  
   return (
     <>
-      <TagSet title={"Choose Tags"} tagState={newInputTagState} setTagState={setNewInputTagState} />
+      <TagSet key={keyMod + "a"} title={"Choose Tags"} tagState={newInputTagState} setTagState={setNewInputTagState} keyMod={keyMod} />
 
       <label className="label-box">
         Add new tags as a List (separated by spaces or commas)
         <InputText
+          key={keyMod + "b"}
           placeholder="E.G.: JavaScript, Fundamentals"
           onInput={updateCustomTags}
           defaultValue={inputFormStateTagsString}
