a5a690dcdf2e65b441e9c7505757803c64f558bd
GLD5000 <garethlouisdevlin@gmail.com>
2022-11-29 16:54:58 +0000

    Moved constants out of inputField

--- src/components/header/AddTip/InputField.js
+++ src/components/header/AddTip/InputField.js
@@ -1,3 +1,33 @@
+const placeHolderObject = {
+  text: `  
+  Type or paste some text here...`,
+  hint: `  
+  Type or paste some text here...`,
+  code: `
+  Type or paste some code here e.g:
+
+      function add(a, b){
+
+          return a + b;
+          
+      }`,
+  table: `
+  Type or paste comma separated values (CSV) here e.g.
+
+      Column Header A,Column Header B,Column Header C
+      First Row A,First Row B,First Row C
+      Second Row A,Second Row B,Second Row C
+      Third Row A,Third Row B,Third Row C   `,
+};
+const informationLookup = {
+  text: "You can use some basic markdown to enhance your text, use the buttons above to help you!",
+  hint: `Type or paste your hint here...`,
+  code: `Type or paste some code here...`,
+  table: `Type or paste comma separated values (CSV) here...`,
+};
+
+
+
 const InputField = ({ type, changeText, name, defaultValue }) => {
   let showInformation = true;
   function handleChange(e) {
@@ -5,36 +35,8 @@ const InputField = ({ type, changeText, name, defaultValue }) => {
     const index = e.target.name;
     changeText({ content: value }, index);
   }
-  const placeHolderObject = {
-    text: `  
-    Type or paste some text here...`,
-    hint: `  
-    Type or paste some text here...`,
-    code: `
-    Type or paste some code here e.g:
-
-        function add(a, b){
-
-            return a + b;
-            
-        }`,
-    table: `
-    Type or paste comma separated values (CSV) here e.g.
-
-        Column Header A,Column Header B,Column Header C
-        First Row A,First Row B,First Row C
-        Second Row A,Second Row B,Second Row C
-        Third Row A,Third Row B,Third Row C   `,
-  };
-
   const placeHolder = placeHolderObject[type];
   function getInformation(type) {
-    const informationLookup = {
-      text: "You can use some basic markdown to enhance your text, use the buttons above to help you!",
-      hint: `Type or paste your hint here...`,
-      code: `Type or paste some code here...`,
-      table: `Type or paste comma separated values (CSV) here...`,
-    };
     return informationLookup[type];
   }
   const information = getInformation(type);
