694e4632e4fc67eb2712bd9dbd6b2841188a69c5
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-14 13:49:32 +0000

    Added filename title to codebox

--- src/elements/CodeBox.js
+++ src/elements/CodeBox.js
@@ -12,21 +12,29 @@ export default function CodeBox ({content, parse = false}) {
   // console.log(content);
   const code = parse? removeParagraphs(content): content;
   const [showTextState, setShowTextState] = useState(() => false);
+const firsLineBreak = code.search(/\r?\n/);
+const isLineBreak = firsLineBreak > -1;
+const codeFileName = isLineBreak? code.slice(0,firsLineBreak): null;
+  const codeBody = isLineBreak? code.slice(firsLineBreak + 1): code;
   return (
     <code className="code">
-      {`${code}`}
-      <SvgButton
-            type="duplicate"
-            key={1 + "copyCode"}
-            text="Copied!"
-            clickFunction={()=> {if (code.includes("Pp")) alert(code);navigator.clipboard.writeText(code); setShowTextState(true); setTimeout(() => setShowTextState(false), 2200)}}
-            wide = {true}
-            showText = {showTextState}
-            borderColor = "transparent"
-            reverse = {false}
-            classes = "copy-btn"
-            marginLeft = "0"
-      />
+      {`${codeBody}`}
+      <label>
+
+        <SvgButton
+              type="duplicate"
+              key={1 + "copyCode"}
+              text="Copied!"
+              clickFunction={()=> {if (codeBody.includes("Pp")) alert(codeBody);navigator.clipboard.writeText(codeBody); setShowTextState(true); setTimeout(() => setShowTextState(false), 2200)}}
+              wide = {true}
+              showText = {showTextState}
+              borderColor = "transparent"
+              reverse = {false}
+              classes = "copy-btn"
+              marginLeft = "0"
+              />
+              <h3 className="file-name">{codeFileName}</h3>
+      </label>
 
       </code>
   )
