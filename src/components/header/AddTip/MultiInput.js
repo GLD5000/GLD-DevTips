fcfa43f16ba99a61308249c297a8033523086426
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-15 16:33:42 +0000

    Added autofocus on copy section

--- src/components/header/AddTip/MultiInput.js
+++ src/components/header/AddTip/MultiInput.js
@@ -1,8 +1,9 @@
 import InputField from "./InputField";
 import Button from "../../../elements/Button";
 import InputButtons from "./InputButtons";
-import SvgButton from "../../../elements/SvgButton"
+import SvgButton from "../../../elements/SvgButton";
 let keyInc = 0;
+let autoFocusIndex = null;
 const MultiInput = ({
   authClickHandler,
   onSubmit,
@@ -12,9 +13,14 @@ const MultiInput = ({
   isOwner,
   setInputFormState,
 }) => {
-
-  function deepCloneInputFormState(){
-    return {...inputFormState, sections: inputFormState.sections.map(x => {return{...x}}), tags: [...inputFormState.tags]};
+  function deepCloneInputFormState() {
+    return {
+      ...inputFormState,
+      sections: inputFormState.sections.map((x) => {
+        return { ...x };
+      }),
+      tags: [...inputFormState.tags],
+    };
   }
   function addField() {
     setInputFormState((object) => {
@@ -32,26 +38,30 @@ const MultiInput = ({
     incrementKeys();
     const index = getIndexOfE(e);
     const newObject = deepCloneInputFormState();
-    const duplicateObject = {...newObject.sections[index]};
-    newObject.sections[index].title = newObject.sections[index].title === undefined? `Copy of section ${index +1}`: `${newObject.sections[index].title} (copy)`;
+    const duplicateObject = { ...newObject.sections[index] };
+    autoFocusIndex = index + 1;
+    newObject.sections[index].title =
+      newObject.sections[index].title === undefined
+        ? `Copy of section ${index + 1}`
+        : `${newObject.sections[index].title} (copy)`;
     newObject.sections.splice(index, 0, duplicateObject);
 
     setInputFormState(() => {
       return newObject;
     });
   }
-  function swapArrayPositions(array, index, direction = "up"){
-    const indexModifier = direction === "down"? 1: -1;
+  function swapArrayPositions(array, index, direction = "up") {
+    const indexModifier = direction === "down" ? 1 : -1;
     const secondIndex = index + indexModifier;
-    const indexLimit = array.length -1;
+    const indexLimit = array.length - 1;
     if (secondIndex > indexLimit || secondIndex < 0) return array;
     [array[index], array[secondIndex]] = [array[secondIndex], array[index]];
-    return array
+    return array;
   }
-  function incrementKeys(){
+  function incrementKeys() {
     keyInc += 1;
   }
-  function moveFieldUp(e){
+  function moveFieldUp(e) {
     incrementKeys();
     const index = getIndexOfE(e);
     const newObject = deepCloneInputFormState();
@@ -61,9 +71,8 @@ const MultiInput = ({
       newObject.sections = newSections;
       return newObject;
     });
-
   }
-  function moveFieldDown(e){
+  function moveFieldDown(e) {
     incrementKeys();
     const index = getIndexOfE(e);
     const newObject = deepCloneInputFormState();
@@ -73,9 +82,8 @@ const MultiInput = ({
       newObject.sections = newSections;
       return newObject;
     });
-
   }
-  function getIndexOfE(e){
+  function getIndexOfE(e) {
     return parseInt(e.target.id.split("-")[0]);
   }
   function deleteIndexedField(e) {
@@ -116,13 +124,13 @@ const MultiInput = ({
     });
   }
   function makeInputArray() {
-    return inputFormState.sections.map((object, index, arr) => {
-      const autoFocus = index === arr.length -1;
+    return inputFormState.sections.map((object, index) => {
+      const autoFocus = index === autoFocusIndex;
       const returnObject = (
-        <div key={(keyInc + "a" + index)} className="field-container">
+        <div key={keyInc + "a" + index} className="field-container">
           <h2>Section {index + 1}</h2>
           <InputButtons
-            key={(keyInc + "a" + index) + "InputButtons"}
+            key={keyInc + "a" + index + "InputButtons"}
             type={object.type}
             index={index}
             changeValue={changeValue}
@@ -131,7 +139,7 @@ const MultiInput = ({
             autoFocus={autoFocus}
           />
           <InputField
-            key={(keyInc + "a" + index) + "InputField"}
+            key={keyInc + "a" + index + "InputField"}
             id={index + "InputField"}
             name={index}
             type={object.type}
@@ -139,57 +147,58 @@ const MultiInput = ({
             changeText={changeValue}
           />
           <div className="svg-btn-grid">
+            <SvgButton
+              type="up"
+              key={index + "moveFieldUp"}
+              text="Up"
+              clickFunction={moveFieldUp}
+              id={index + "-moveFieldUp"}
+            />
+            <SvgButton
+              type="down"
+              key={index + "moveFieldDown"}
+              backgroundColor="transparent"
+              text="Down"
+              clickFunction={moveFieldDown}
+              id={index + "-moveFieldDown"}
+            />
 
-          <SvgButton
-            type="up"
-            key={index + "moveFieldUp"}
-            text="Up"
-            clickFunction={moveFieldUp}
-            id={index +"-moveFieldUp"}
-          />
-          <SvgButton
-            type="down"
-            key={index + "moveFieldDown"}
-            backgroundColor="transparent"
-            text="Down"
-            clickFunction={moveFieldDown}
-            id={index +"-moveFieldDown"}
-          />
-
-
-          <SvgButton
-            type="duplicate"
-            key={index + "duplicateField"}
-            text="Copy"
-            clickFunction={duplicateField}
-            id={index +"-duplicatefield"}
-          />
-          <SvgButton
-            type="delete"
-            key={index + "deleteIndexedField"}
-            text="Delete"
-            clickFunction={deleteIndexedField}
-            id={index +"-deleteIndexedField"}
-          />
+            <SvgButton
+              type="duplicate"
+              key={index + "duplicateField"}
+              text="Copy"
+              clickFunction={duplicateField}
+              id={index + "-duplicatefield"}
+            />
+            <SvgButton
+              type="delete"
+              key={index + "deleteIndexedField"}
+              text="Delete"
+              clickFunction={deleteIndexedField}
+              id={index + "-deleteIndexedField"}
+            />
           </div>
-
-
         </div>
       );
-
       return returnObject;
     });
   }
-  function signedInNonOwner(){
+  function signedInNonOwner() {
     alert("You are not allowed to submit to the database- sorry!");
-  };
+  }
   const inputArray = makeInputArray();
   const submitText = isOwner
     ? "Save"
-    : signedIn? "Save (Wrong User)": "Sign in to Save";
+    : signedIn
+    ? "Save (Wrong User)"
+    : "Sign in to Save";
   const submitBackColour = isOwner ? "green" : "silver";
   const submitTextColour = isOwner ? "white" : "black";
-  const submitFunction = isOwner ? onSubmit: signedIn? signedInNonOwner: authClickHandler;
+  const submitFunction = isOwner
+    ? onSubmit
+    : signedIn
+    ? signedInNonOwner
+    : authClickHandler;
 
   return (
     <>
