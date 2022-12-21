0ad59b895cb1431f1cd99ebf563c1b45d73ef1c3
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-21 15:43:39 +0000

    Added display:block and updated filename regex

--- src/elements/CodeBox.js
+++ src/elements/CodeBox.js
@@ -11,36 +11,36 @@ export default function CodeBox({ content, parse = false }) {
   // console.log(content);
   const code = parse ? removeParagraphs(content) : content;
   const [showTextState, setShowTextState] = useState(() => false);
-  const firsLineBreak = code.search(/\r?\n/);
-  const isLineBreak = firsLineBreak > -1;
+  const firstLineBreak = code.search(/\r?\n/);
+  const isLineBreak = firstLineBreak > -1;
   const codeFileName =
-    isLineBreak && code.search(/^[*\w.]+(?=(\r?\n))/) > -1
-      ? code.slice(0, firsLineBreak)
+    isLineBreak && code.search(/^[*\w]+\.[*\w]+(?=(\r?\n))/) > -1
+      ? code.slice(0, firstLineBreak)
       : null;
-  const codeBody = codeFileName ? code.slice(firsLineBreak + 1) : code;
+  const codeBody = codeFileName ? code.slice(firstLineBreak + 1) : code;
   return (
-    <code className="bg-black border-neutral-500 text-vsGreen rounded border-solid border font-mono whitespace-pre overflow-x-auto">
+    <code className="block bg-black border-neutral-500 text-vsGreen rounded border-solid border font-mono whitespace-pre overflow-x-auto">
+      <div className="grid grid-cols-3 justify-items-end">
+        <h3 className="file-name p-3 col-start-2">{codeFileName}</h3>
+          <SvgButton
+            type="duplicate"
+            key={1 + "copyCode"}
+            text="Copied!"
+            clickFunction={() => {
+              if (codeBody.includes("Pp")) alert(codeBody);
+              navigator.clipboard.writeText(codeBody);
+              setShowTextState(true);
+              setTimeout(() => setShowTextState(false), 2200);
+            }}
+            wide={true}
+            showText={showTextState}
+            borderColor="transparent"
+            reverse={true}
+            classes="copy-btn col-start-3"
+            // marginLeft="auto"
+          />
+      </div>
       {`${codeBody}`}
-      <label>
-        <SvgButton
-          type="duplicate"
-          key={1 + "copyCode"}
-          text="Copied!"
-          clickFunction={() => {
-            if (codeBody.includes("Pp")) alert(codeBody);
-            navigator.clipboard.writeText(codeBody);
-            setShowTextState(true);
-            setTimeout(() => setShowTextState(false), 2200);
-          }}
-          wide={true}
-          showText={showTextState}
-          borderColor="transparent"
-          reverse={false}
-          classes="copy-btn"
-          marginLeft="0"
-        />
-        <h3 className="file-name">{codeFileName}</h3>
-      </label>
     </code>
   );
 }
