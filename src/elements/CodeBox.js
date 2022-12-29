1bea70873091078b19951134114ce845c302f0f7
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-29 12:31:03 +0000

    Swapped layout

--- src/elements/CodeBox.js
+++ src/elements/CodeBox.js
@@ -19,26 +19,26 @@ export default function CodeBox({ content, parse = false }) {
       : null;
   const codeBody = codeFileName ? code.slice(firstLineBreak + 1) : code;
   return (
-    <code className="block bg-black border-neutral-500 text-vsGreen rounded border-solid border font-mono whitespace-pre overflow-x-auto">
-      <div className="grid grid-cols-3 justify-items-end">
-        <h3 className="file-name p-3 col-start-2">{codeFileName}</h3>
-          <SvgButton
-            type="duplicate"
-            key={1 + "copyCode"}
-            text="Copied!"
-            clickFunction={() => {
-              if (codeBody.includes("Pp")) alert(codeBody);
-              navigator.clipboard.writeText(codeBody);
-              setShowTextState(true);
-              setTimeout(() => setShowTextState(false), 2200);
-            }}
-            wide={true}
-            showText={showTextState}
-            reverse={true}
-            buttonClasses="border-transparent sticky left-0 col-start-3"
-            // marginLeft="auto"
-            svgClasses="stroke-1 fill-black stroke-whitesmoke"
-          />
+    <code className="block overflow-x-auto whitespace-pre rounded border border-solid border-neutral-500 bg-black font-mono text-vsGreen">
+      <div className="grid grid-cols-autoFr justify-items-end">
+        <SvgButton
+          type="duplicate"
+          key={1 + "copyCode"}
+          text="Copied!"
+          clickFunction={() => {
+            if (codeBody.includes("Pp")) alert(codeBody);
+            navigator.clipboard.writeText(codeBody);
+            setShowTextState(true);
+            setTimeout(() => setShowTextState(false), 2200);
+          }}
+          wide={true}
+          showText={showTextState}
+          reverse={false}
+          buttonClasses="border-transparent sticky left-0"
+          // marginLeft="auto"
+          svgClasses="stroke-1 fill-black stroke-whitesmoke"
+        />
+        <h3 className="file-name p-3">{codeFileName}</h3>
       </div>
       {`${codeBody}`}
     </code>
