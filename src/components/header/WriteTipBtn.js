ec6a1f70efdbbf12621ebac7461e6a88a50cb1cb
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-29 12:29:15 +0000

    Updated styling

--- src/components/header/WriteTipBtn.js
+++ src/components/header/WriteTipBtn.js
@@ -1,75 +1,44 @@
-import SvgButton from "../../elements/SvgButton"
-import { useState } from "react";
-
-// const colourLookup = {
-//     false: {colour: "whitesmoke", background: "transparent"},
-//     true: {colour: "whitesmoke", background: "transparent"},
-// }
-const colourLookupHover = {
-    true: {colour: "text-black", background: "text-pink"},
-    false: {colour: "bg-black", background: "bg-aquamarine"},
-}
-
+import SvgButton from "../../elements/SvgButton";
+const writeClasses =
+  " hover:bg-aquamarine hover:text-black text-aquamarine bg-transparent";
+const cancelClasses =
+  "bg-transparent text-pink-300 hover:bg-pink-300 hover:text-black";
 
 export default function WriteTipBtn({
-    showAddTipForm,
-    setShowAddTipForm,
-    addObjectToInputFormState,
-    setSearchQuery,
-    clearTags,
+  showAddTipForm,
+  setShowAddTipForm,
+  addObjectToInputFormState,
+  setSearchQuery,
+  clearTags,
 }) {
-    
-    const [buttonState, setButtonState] = useState(() => {
-        return {
-            colour: showAddTipForm ? "text-black" : "text-whitesmoke",
-            background: showAddTipForm ? "bg-pink" : "bg-transparent"
-        }
-    });
-    function setButtonStateFromObject(object){
-        setButtonState(() => {
-            return {
-                colour: object.colour,
-                background: object.background
-            }
-        })
+  function onClickAdd() {
+    if (showAddTipForm === true) {
+      onClose();
+      return;
     }
-    
-    function onClickAdd() {
-        if (showAddTipForm === true) {onClose();
-        return;
-        }
-        clearTags();
-        setSearchQuery(()=> "  ");
-        setShowAddTipForm(true);
-        setButtonStateFromObject(colourLookupHover[true]);
-    }
-    function onClose() {
-        addObjectToInputFormState(null);
-        setSearchQuery(() => "")
-        setShowAddTipForm(false);
-        setButtonStateFromObject(colourLookupHover[false]);       
-    }
-    const AddTipText = showAddTipForm
-    ? "Cancel"
-    : "Create";
-    const type = showAddTipForm
-    ? "cancelWrite"
-    : "add";
-    const svgClasses = showAddTipForm? "stroke-1 stroke-whitesmoke fill-none": "stroke-1 stroke-whitesmoke hover:stroke-black fill-none";
-    return (
-        <div  style={{width:"fit-content", gridColumn:"4"}}>
-            
-
-            <SvgButton
-            wide="false"
-            type={type}
-        color={buttonState.colour}
-        backgroundColor={buttonState.background}
+    clearTags();
+    setSearchQuery(() => "  ");
+    setShowAddTipForm(true);
+  }
+  function onClose() {
+    addObjectToInputFormState(null);
+    setSearchQuery(() => "");
+    setShowAddTipForm(false);
+  }
+  const AddTipText = showAddTipForm ? "Cancel" : "Create";
+  const type = showAddTipForm ? "cancelWrite" : "add";
+  return (
+    <div className="height-fill width-fit col-start-4">
+      <SvgButton
+        wide="false"
+        type={type}
+        // color={buttonState.colour}
+        // backgroundColor={buttonState.background}
         text={AddTipText}
         clickFunction={onClickAdd}
         reverse={true}
-        svgClasses={svgClasses}
+        buttonClasses={showAddTipForm ? cancelClasses : writeClasses}
       />
-        </div>
-)
+    </div>
+  );
 }
