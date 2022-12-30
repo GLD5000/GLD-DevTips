cdcc4baa9500d74f3f376e16171b6b6f706b0815
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-30 13:08:59 +0000

    Updated title button styling

--- src/elements/SearchTitle.js
+++ src/elements/SearchTitle.js
@@ -10,12 +10,15 @@ export default function SearchTitle({
   searchQuery,
   listId,
 }) {
-  const placeholder =  " Type or select a title...";
-  const defaultValue = searchQuery.length > 0? searchQuery: null;
+  const placeholder = " Type or select a title...";
+  const defaultValue = searchQuery.length > 0 ? searchQuery : null;
   return (
     <>
-      <button className="tip-title-btn" onClick={onClick}>
-        <h2 className="tip-title-txt">{title}</h2>
+      <button
+        className="grid w-full grid-cols-frAutoFr justify-items-end rounded-none rounded-t-lg border-b border-b-borderGrey hover:bg-slate-800 active:bg-slate-600"
+        onClick={onClick}
+      >
+        <h2 className="col-start-2">{title}</h2>
         {expanded ? (
           <CollapseSvg className="tip-title-svg" />
         ) : (
--- src/elements/TipTitle.js
+++ src/elements/TipTitle.js
@@ -3,8 +3,11 @@ import ExpandSvg from "../components/icons/ExpandSvg";
 
 const TipTitle = ({ title, onClick, expanded }) => {
   return (
-    <button className="tip-title-btn" onClick={onClick}>
-      <h2 className="tip-title-txt">{title}</h2>
+    <button 
+    className="grid w-full grid-cols-frAutoFr justify-items-end rounded-none rounded-t-lg border-b border-b-borderGrey hover:bg-slate-800 active:bg-slate-600"
+    onClick={onClick}
+    >
+      <h2 className="col-start-2">{title}</h2>
       {expanded ? <CollapseSvg className="tip-title-svg" /> : <ExpandSvg className="tip-title-svg" />}
     </button>
   );
