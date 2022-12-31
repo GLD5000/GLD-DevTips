abba641a62fc5a99dbc96f95fe92fbafe9f57ff5
GLD5000 <garethlouisdevlin@gmail.com>
2022-12-31 12:53:31 +0000

    Added local link parser

--- src/elements/Link.js
+++ src/elements/Link.js
@@ -1,25 +1,57 @@
 let linkString; 
+  function sliceLinkLocalUrl(item){
+    const type = "local";
+    
+    const indexOfOpenBracket = item.indexOf("(");
+    const startSlice = indexOfOpenBracket + 1 || false;
+    const urlField = item.slice(startSlice);
+
+    const titleIndex = urlField.indexOf("title");
+    const tagIndex = urlField.indexOf("tags");
+
+    const url = window.location.href; 
+
+    const indexOfClosedSquareBracket = item.indexOf("]")
+    const text =  item.slice(0, indexOfClosedSquareBracket);
+
+
+
+    return {url, text, type};
+
+  }
+  function sliceLinkSimple(item){
+    const type = "web";
+
+    const indexOfOpenBracket = item.indexOf("(");
+    const startSlice = indexOfOpenBracket + 1 || false;
+    const url = item.slice(startSlice);
+
+    const indexOfClosedSquareBracket = item.indexOf("]")
+    const text =  item.slice(0, indexOfClosedSquareBracket);
+
+    return {url, text, type};
+  }
+
   function itemHandler(item){
     if (typeof item !== "string") return item;
     const itemContainsLink = item.includes("http") || item.includes("www");
     if (itemContainsLink === false) return item;
-    const indexOfOpenBracket = item.indexOf("(");
-    const startSlice = indexOfOpenBracket + 1 || false;
-
-    linkString = item.slice(startSlice);
-    const indexOfClosedSquareBracket = item.indexOf("]")
-    return item.slice(0, indexOfClosedSquareBracket);
+    return sliceLinkSimple(item)
   }
 
 
-export default function Link({ content}) {
-  const text  = itemHandler(content);
-  const link = linkString;
+export default function Link({ item}) {
+  const {url, text, type}  = itemHandler(item);
 
   return (<>
-    <a href={link}  target="_blank" rel="noreferrer">
+    {type === "local"?
+    (<a href={url}>
       {text}
-    </a>
+    </a>):
+    (<a href={url}  target="_blank" rel="noreferrer">
+    {text}
+  </a>)
+    }
   </>
   );
 }
