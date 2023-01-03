69608e72ca8718464a79eca636378434b2484973
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-03 13:11:33 +0000

    Added click handler

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,21 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { onAuthStateChanged, signOut as authSignOut } from 'firebase/auth';
-import { auth } from './firebase';
+import { auth as firebaseAuth } from './firebase';
 //additional
 import { getUserRole } from "./firestore"
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+prompt: "select_account",
+});
+
 
 export default function useFirebaseAuth() { // auth function
   const [authUser, setAuthUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  console.count("useFirebaseAuth");
   const clear = () => {
     setAuthUser(null);
     setIsLoading(false);
@@ -30,20 +37,31 @@ export default function useFirebaseAuth() { // auth function
     setIsLoading(false);
   }; 
 
-  const signOut = () => authSignOut(auth).then(clear);
-console.group(`auth.currentUser`);
-console.log(auth.currentUser?.displayName || null);
-console.groupEnd();
+  const signOut = () => authSignOut(firebaseAuth).then(clear);
+  const signIn = () => signInWithPopup(firebaseAuth, provider);
+  const clickHandler = () => {
+    console.count("click handler")
+    if (firebaseAuth.currentUser === null) {
+      signIn();
+      return;
+    } 
+    signOut();
+  };
+
+  // console.group(`firebaseAuth.currentUser`);
+  // console.log(firebaseAuth.currentUser?.displayName || null);
+  // console.groupEnd();
   // Listen for Firebase Auth state change
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, authStateChanged);
+    const unsubscribe = onAuthStateChanged(firebaseAuth, authStateChanged);
     console.count("Firebase Auth state change");
     return () => unsubscribe();
-  }, [auth.currentUser]);
+// eslint-disable-next-line
+  }, [firebaseAuth.currentUser]);
   return {
     authUser,
     isLoading,
-    signOut
+    clickHandler
   };
 }
 
@@ -51,45 +69,43 @@ console.groupEnd();
 const AuthUserContext = createContext({ // context
   authUser: null,
   isLoading: true,
-  signOut: async () => {}
+  clickHandler: async () => {}
 });
 export function AuthUserProvider({ children }) { // context provider
-  const auth = useFirebaseAuth();
-  console.log(auth.authUser);
-  return <AuthUserContext.Provider value={auth}>{children}</AuthUserContext.Provider>;
+  console.count("AuthUserProvider");
+
+  const appAuth = useFirebaseAuth();
+  // console.log(appAuth.authUser);
+  console.group(`appAuth`);
+  console.log(appAuth);
+  console.groupEnd();
+  return <AuthUserContext.Provider value={appAuth}>{children}</AuthUserContext.Provider>;
 }
 
 export const useAuth = () => useContext(AuthUserContext); // custom hook
 
 //additional
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-  prompt: "select_account",
-});
+// const provider = new GoogleAuthProvider();
+// provider.setCustomParameters({
+//   prompt: "select_account",
+// });
 
-async function checkRole(user, setSignedIn, setIsOwner) {
-  console.log("checkRole");
+// async function authClickHandler(appAuth) {
+
+//   if (appAuth !== null) {
+//     await appAuth.signOut();
+//   } else if (appAuth === null) {
+//     await signInWithPopup(firebaseAuth, provider);
+//   }
+// }
+
+
+async function checkRole(user) {
+  console.count("checkRole");
   const uid = user.uid;
   const role = await getUserRole(uid);
   console.log(`got role ${role}`);
   const isOwner = role === "owner";
-  if (isOwner) {
-    // console.log("Is owner = " + isOwner);
-    // console.log("signing in...");
-    // setSignedIn(true);
-
-    // setIsOwner(true);
-  } else {
-    // console.log("signing in...");
-    // setSignedIn(true);
-    // setIsOwner(false);
-
-    // console.log(
-    //   "You are not authorised to submit tips to the database- sorry!"
-    // );
-
-    // setIsOwner(false);
-  }
   return isOwner;
 }
