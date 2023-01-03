32496f3ed5dcc1840ed6436a8c883aee84bfc00a
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-03 16:00:47 +0000

    Removed console.log

--- src/components/header/AuthButton.js
+++ src/components/header/AuthButton.js
@@ -2,7 +2,6 @@ import Button from "../../elements/Button";
 import { useAuth } from "../../auth";
 export default function AuthButton() {
   const appAuth = useAuth();
-   console.log(appAuth); 
   const signedIn = appAuth.authUser !== null;
   const text = signedIn ? "Log Out " + appAuth.authUser.name : "Log In";
   const className =
