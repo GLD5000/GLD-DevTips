fb9365ca1c4d746beb333f2823144c656ca4d7c8
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-03 15:17:40 +0000

    Updated auth useEffect

--- src/auth.js
+++ src/auth.js
@@ -1,111 +1,95 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import { onAuthStateChanged, signOut as authSignOut } from 'firebase/auth';
-import { auth as firebaseAuth } from './firebase';
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, signOut as authSignOut } from "firebase/auth";
+import { auth as firebaseAuth } from "./firebase";
 //additional
-import { getUserRole } from "./firestore"
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getUserRole } from "./firestore";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-prompt: "select_account",
+  prompt: "select_account",
 });
 
-
-export default function useFirebaseAuth() { // auth function
+export default function useFirebaseAuth() {
+  // auth function
   const [authUser, setAuthUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  console.count("useFirebaseAuth");
+  // console.count("useFirebaseAuth");
   const clear = () => {
     setAuthUser(null);
     setIsLoading(false);
   };
 
-  const authStateChanged = async (user) => {
-    setIsLoading(true);
-    if (!user) {
-        clear();
-        return;
-    }
-    
-    setAuthUser({
-        uid: user.uid,
-        email: user.email,
-        isOwner: await checkRole(user),
-        name: user.displayName
-    });
-    setIsLoading(false);
-  }; 
-
   const signOut = () => authSignOut(firebaseAuth).then(clear);
   const signIn = () => signInWithPopup(firebaseAuth, provider);
   const clickHandler = () => {
-    console.count("click handler")
+    console.count("click handler");
     if (firebaseAuth.currentUser === null) {
       signIn();
       return;
-    } 
+    }
     signOut();
   };
 
-  // console.group(`firebaseAuth.currentUser`);
-  // console.log(firebaseAuth.currentUser?.displayName || null);
-  // console.groupEnd();
-  // Listen for Firebase Auth state change
   useEffect(() => {
+    const authStateChanged = async (user) => {
+      console.count("auth changed state");
+      setIsLoading(true);
+      if (!user) {
+        clear();
+        return;
+      }
+
+      setAuthUser({
+        uid: user.uid,
+        email: user.email,
+        isOwner: await checkRole(user),
+        name: user.displayName,
+      });
+      setIsLoading(false);
+    };
+
     const unsubscribe = onAuthStateChanged(firebaseAuth, authStateChanged);
-    console.count("Firebase Auth state change");
     return () => unsubscribe();
-// eslint-disable-next-line
-  }, [firebaseAuth.currentUser]);
+  }, []);
   return {
     authUser,
     isLoading,
-    clickHandler
+    clickHandler,
   };
 }
 
-
-const AuthUserContext = createContext({ // context
+const AuthUserContext = createContext({
+  // context
   authUser: null,
   isLoading: true,
-  clickHandler: async () => {}
+  clickHandler: async () => {},
 });
-export function AuthUserProvider({ children }) { // context provider
-  console.count("AuthUserProvider");
+export function AuthUserProvider({ children }) {
+  // context provider
+  // console.count("AuthUserProvider");
 
   const appAuth = useFirebaseAuth();
   // console.log(appAuth.authUser);
-  console.group(`appAuth`);
-  console.log(appAuth);
-  console.groupEnd();
-  return <AuthUserContext.Provider value={appAuth}>{children}</AuthUserContext.Provider>;
+  // console.group(`appAuth`);
+  // console.log(appAuth);
+  // console.groupEnd();
+  return (
+    <AuthUserContext.Provider value={appAuth}>
+      {children}
+    </AuthUserContext.Provider>
+  );
 }
 
 export const useAuth = () => useContext(AuthUserContext); // custom hook
 
-//additional
-
-// const provider = new GoogleAuthProvider();
-// provider.setCustomParameters({
-//   prompt: "select_account",
-// });
-
-// async function authClickHandler(appAuth) {
-
-//   if (appAuth !== null) {
-//     await appAuth.signOut();
-//   } else if (appAuth === null) {
-//     await signInWithPopup(firebaseAuth, provider);
-//   }
-// }
-
 
 async function checkRole(user) {
-  console.count("checkRole");
+  // console.count("checkRole");
   const uid = user.uid;
   const role = await getUserRole(uid);
-  console.log(`got role ${role}`);
+  // console.log(`got role ${role}`);
   const isOwner = role === "owner";
   return isOwner;
 }
