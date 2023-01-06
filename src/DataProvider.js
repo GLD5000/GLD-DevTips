50089a3bad936097b3d0fb68b21bc2932ca54bda
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-06 11:02:27 +0000

    Split filter state and updated function names

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -1,41 +1,38 @@
 import { useState, useEffect, createContext, useContext } from "react";
-import { getTagsFirestore, getTipsFirestore} from "./firestore";
+import { getTagsFirestore, getTipsFirestore } from "./firestore";
 const url = window.location.search;
 const urlObject = new URLSearchParams(url);
 const searchFromUrl = urlObject.get("title");
 const tagsFromUrl = urlObject.getAll("tags").map((x) => x.toLowerCase());
-function activateTagsFromArray(tags, array){
-  console.group(`array`);
-  console.log(array);
-  console.groupEnd();
-  array.forEach(tagId => tags[tagId.toLowerCase()].active = true);
-  return tags;
-}
 
 function useData() {
+  const [filterActive, setFilterActive] = useState(false);
+  const [searchString, setSearchString] = useState(searchFromUrl || "");
   const [uiMode, setUiMode] = useState(() => {
     return { edit: false, tags: false };
   });
-  const [filters, setFilter] = useState(() => {
-      return { active: false, searchString: searchFromUrl || "" };
-    });
+  const [inputForm, setInputForm] = useState(null);
   const [tags, setTags] = useState(null);
   const [tips, setTips] = useState(null);
-  const [inputForm, setInputForm] = useState(null);
-
 
   useEffect(() => {
     let runEffect = true;
     getTagsFirestore().then((result) => {
       if (runEffect) {
-        if (tagsFromUrl.length > 0) activateTagsFromArray(result, tagsFromUrl);
-        console.log(result);
+        if (tagsFromUrl.length > 0)
+          activateTagsFromArray(
+            result,
+            tagsFromUrl,
+            filterActive,
+            setFilterActive
+          );
+        // console.log(result);
         setTags(result);
       }
     });
     getTipsFirestore().then((result) => {
       if (runEffect) {
-        console.log(result);
+        // console.log(result);
         setTips(result);
       }
     });
@@ -44,37 +41,55 @@ function useData() {
     };
   }, []);
 
-  function tagClick(tagId){}
-  function searchChange(searchString){
-    setFilter((stateObject) => {
-      return {...stateObject, searchString: searchString};
-
-    });
+  function clickTag(tagId) {}
+  function changeSearch(searchString) {
+    setSearchString(searchString);
   }
-  function previewTip(){}
-  function saveTip(){}
-  function editTip(){}
-  function deleteTip(){}
-
+  function previewTip() {}
+  function saveTip() {}
+  function editTip() {}
+  function deleteTip() {}
 
   return {
-    uiMode, //edit: true, tags: true
+    uiMode,
     inputForm,
     tags,
-    filters,
+    filterActive,
+    searchString,
     tips,
     setUiMode,
-    tagClick,
-    searchChange,
+    setInputForm,
+    clickTag,
+    changeSearch,
     previewTip,
     saveTip,
     editTip,
     deleteTip,
   };
 }
-
+let log = true;
 export default function DataProvider({ children }) {
   const data = useData();
+  // useEffect(() => {
+  if (log === true && data.tags !== null) {
+    console.table(
+      "filterActive",
+      data.filterActive,
+      "searchString",
+      data.searchString,
+      "uiMode",
+      data.uiMode,
+      "inputForm",
+      data.inputForm,
+      "tags",
+      data.tags,
+      "tips",
+      data.tips
+    );
+    log = false;
+  }
+
+  // },[])
 
   return <dataContext.Provider value={data}>{children}</dataContext.Provider>;
 }
@@ -85,8 +100,8 @@ const dataContext = createContext({
   filters: null,
   tips: null,
   inputForm: null,
-  tagClick: async () => {},
-  searchChange: async () => {},
+  clickTag: async () => {},
+  changeSearch: async () => {},
   previewTip: async () => {},
   saveTip: async () => {},
   editTip: async () => {},
@@ -94,3 +109,16 @@ const dataContext = createContext({
 });
 
 export const useDataContext = () => useContext(dataContext); // custom hook
+
+function activateTagsFromArray(tags, array, filterActive, setFilterActive) {
+  // console.group(`array`);
+  // console.log(array);
+  // console.groupEnd();
+  array.forEach((tagId) => {
+    const key = tagId.toLowerCase();
+    if (tags[key] === undefined) return;
+    if (filterActive === false) setFilterActive(true);
+    tags[key].active = true;
+  });
+  return tags;
+}
