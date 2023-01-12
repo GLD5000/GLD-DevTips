26ad1c260d9a0c8efbec64e6afd3b72e9139df33
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-12 20:27:51 +0000

    Deleted unneeded code

--- src/contexts/CombinedProviders.js
+++ src/contexts/CombinedProviders.js
@@ -1,30 +1,19 @@
 import { AuthUserProvider } from "../auth";
 import AppDataProvider from "./Providers/AppDataProvider";
 import FilteredDataProvider from "./Providers/FilteredDataProvider";
+import InputContextProvider from "./Providers/InputFormProvider";
 
-
-export default function CombinedProviders({children}) {
-    function getNestedArray(children){
-
-        const importArray = [
-            AppDataProvider,
-            FilteredDataProvider
-        ];
-        return importArray.reduceRight(reducer, children);
-
-        function reducer(children, currentElement){
-            return currentElement({children});
-        }
-
-    }
-    // console.log(getNestedArray(children));
-    return (
+export default function CombinedProviders({ children }) {
+    
+  return (
     <AuthUserProvider>
-        <AppDataProvider>
-            <FilteredDataProvider>
-                {children}
-            </FilteredDataProvider>
-        </AppDataProvider>
+      <AppDataProvider>
+        <FilteredDataProvider>
+          <InputContextProvider>
+            {children}
+          </InputContextProvider>
+        </FilteredDataProvider>
+      </AppDataProvider>
     </AuthUserProvider>
-  )
+  );
 }
