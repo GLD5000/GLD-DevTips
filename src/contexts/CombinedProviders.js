24c985708875e944ca8e408b6ab1ed0c242dca05
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-12 22:20:27 +0000

    Updated input form name

--- src/contexts/CombinedProviders.js
+++ src/contexts/CombinedProviders.js
@@ -1,17 +1,14 @@
 import { AuthUserProvider } from "../auth";
 import AppDataProvider from "./Providers/AppDataProvider";
 import FilteredDataProvider from "./Providers/FilteredDataProvider";
-import InputContextProvider from "./Providers/InputFormProvider";
+import InputFormProvider from "./Providers/InputFormProvider";
 
 export default function CombinedProviders({ children }) {
-    
   return (
     <AuthUserProvider>
       <AppDataProvider>
         <FilteredDataProvider>
-          <InputContextProvider>
-            {children}
-          </InputContextProvider>
+          <InputFormProvider>{children}</InputFormProvider>
         </FilteredDataProvider>
       </AppDataProvider>
     </AuthUserProvider>
