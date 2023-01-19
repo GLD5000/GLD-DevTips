fe2a0fdd761494ba0ce9e27fcc67f354cdb96c4d
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-19 17:43:56 +0000

    import tips context

--- src/contexts/Providers/InputFormProvider.js
+++ src/contexts/Providers/InputFormProvider.js
@@ -1,7 +1,8 @@
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useReducer } from "react";
+import { useTipsContext } from "./TipsProvider";
 
 export default function InputFormProvider({ children }) {
-  const inputPointer = useData();
+    const inputPointer = useData();
 
   return (
     <InputFormContext.Provider value={inputPointer}>
@@ -14,15 +15,27 @@ function useData() {
   const [inputTip, setInputTip] = useState(null);
   const [inputPointer, setInputPointer] = useState(null);
 
+
+  const { tips } = useTipsContext();
+  const [inputForm, dispatchInputForm] = useReducer(inputFormReducer, {data: {}, metadata: {inputPointer: -1}})
   return {
-    inputPointer,
-    setInputPointer,
-    inputTip,
-    setInputTip,
+    inputForm, dispatchInputForm
   };
 }
 const InputFormContext = createContext();
 
 export function useInputFormContext() {
   return useContext(InputFormContext);
+}
+
+function inputFormReducer(state, action){
+  const oldData = {...state.data};
+  const oldMetadata = {...state.metadata};
+
+  switch (action.type){
+    case "SET_POINTER":{
+      return {data: oldData, metadata: oldMetadata};
+    }
+  }
+
 }
