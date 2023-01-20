06c5bebf4b596e6f8f4ca39ad705506dfe8bdd89
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-20 00:08:22 +0000

    Added switch cases

--- src/contexts/Providers/InputFormProvider.js
+++ src/contexts/Providers/InputFormProvider.js
@@ -1,25 +1,56 @@
-import { useState, createContext, useContext, useReducer } from "react";
+import { useEffect, createContext, useContext, useReducer } from "react";
 import { useTipsContext } from "./TipsProvider";
 
-export default function InputFormProvider({ children }) {
-    const inputPointer = useData();
+const inputFormStarter = {
+  title: null,
+  id: null,
+  sections: [{ type: "text", content: "" }],
+  tags: [],
+  date: null,
+};
 
+
+export default function InputFormProvider({ children }) {
+  const data = useData();
+  console.group(`data`);
+  console.log(data);
+  console.groupEnd();
   return (
-    <InputFormContext.Provider value={inputPointer}>
+    <InputFormContext.Provider value={data}>
       {children}
     </InputFormContext.Provider>
   );
 }
 
 function useData() {
-  const [inputTip, setInputTip] = useState(null);
-  const [inputPointer, setInputPointer] = useState(null);
+  const {
+    tips: { data: tips },
+    tips: {
+      metadata: { nextTipId },
+    },
+  } = useTipsContext();
+  const [inputForm, dispatchInputForm] = useReducer(inputFormReducer, {
+    data: inputFormStarter,
+    metadata: { inputPointer: -1, editing: false, nextTipId: nextTipId, tips: tips },
+  });
 
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      dispatchInputForm({
+        type: "UPDATE_TIPS",
+        payload: { nextTipId: nextTipId, tips: tips },
+      });
+    }
 
-  const { tips } = useTipsContext();
-  const [inputForm, dispatchInputForm] = useReducer(inputFormReducer, {data: {}, metadata: {inputPointer: -1}})
+    return () => {
+      isMounted = false;
+    };
+  }, [nextTipId, tips]);
+
   return {
-    inputForm, dispatchInputForm
+    inputForm,
+    dispatchInputForm,
   };
 }
 const InputFormContext = createContext();
@@ -28,14 +59,53 @@ export function useInputFormContext() {
   return useContext(InputFormContext);
 }
 
-function inputFormReducer(state, action){
-  const oldData = {...state.data};
-  const oldMetadata = {...state.metadata};
+function inputFormReducer(state, action) {
+  const oldData = { ...state.data };
+  const oldMetadata = { ...state.metadata };
 
-  switch (action.type){
-    case "SET_POINTER":{
-      return {data: oldData, metadata: oldMetadata};
+  switch (action.type) {
+    case "EDIT_TIP": {
+      const tipToEdit = oldMetadata.tips[action.payload];
+      return {
+        data: tipToEdit,
+        metadata: {
+          ...oldMetadata,
+          inputPointer: action.payload,
+          editing: true
+        },
+      };
     }
-  }
+    case "UPDATE_TIPS": {
+      return {
+        data: oldData,
+        metadata: {
+          ...oldMetadata,
+          nextTipId: action.payload.nextTipId,
+          tips: action.payload.tips,
+        },
+      };
+    }
+    default: 
+    case "NEW_TIP": {
+      return {
+        data: inputFormStarter,
+        metadata: {
+          ...oldMetadata,
+          inputPointer: -1,
+          editing: true
+        },
+      };
+    }
+    case "CANCEL_EDIT": {
+      return {
+        data: inputFormStarter,
+        metadata: {
+          ...oldMetadata,
+          inputPointer: -1,
+          editing: false
+        },
+      };
+    }
 
+  }
 }
