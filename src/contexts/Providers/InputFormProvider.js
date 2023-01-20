e7486f0bcc9549505e504edf7a44f10ec013be7d
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-20 22:44:24 +0000

    Refacored and added cases to switch

--- src/contexts/Providers/InputFormProvider.js
+++ src/contexts/Providers/InputFormProvider.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useReducer } from "react";
 import { useTipsContext } from "./TipsProvider";
-import formattedDate from "../../utilities/formattedDate"
+import formattedDate from "../../utilities/formattedDate";
 
 const dataStarter = {
   title: null,
@@ -9,12 +9,13 @@ const dataStarter = {
   tags: [],
 };
 const metadataStarter = {
+  focusId: "",
   preview: false,
   editing: false,
   existingTagsSet: new Set(),
   newTagsArray: [],
   date: null,
-  currentTipId: -1
+  currentTipId: -1,
 };
 
 export default function InputFormProvider({ children }) {
@@ -49,31 +50,16 @@ function useData() {
     const oldMetadata = { ...state.metadata };
 
     switch (action.type) {
-      case "SAVE_TIP": {
-        console.log(dispatchTips);
-        return;
-      }
-      case "PREVIEW_TIP": {
-        console.log(dispatchTips);
-        return;
-      }
-      case "TOGGLE_TAG": {
-        if (action.payload.active) oldMetadata.existingTagsSet.add(action.payload.id);
-        if (!action.payload.active) oldMetadata.existingTagsSet.delete(action.payload.id);
-        oldData.tags = [...oldMetadata.existingTagsSet, ...oldMetadata.newTagsArray];
-        return { data: oldData, metadata: oldMetadata };
-      }
-      case "CLEAR_TAGS": {
-        oldMetadata.existingTagsSet.clear();
-        oldData.tags = [...oldMetadata.existingTagsSet, ...oldMetadata.newTagsArray];
-        return { data: oldData, metadata: oldMetadata };
-      }
-      case "UPDATE_NEW_TAGS": {
-        const newString = action.payload;
-        oldData.newTagsArray =
-          newString.length === 0 ? [] : newString.split(" ");
-
-        return { data: oldData, metadata: oldMetadata };
+      default:
+      case "NEW_TIP": {
+        return {
+          data: dataStarter,
+          metadata: {
+            ...oldMetadata,
+            inputPointer: -1,
+            editing: true,
+          },
+        };
       }
       case "EDIT_TIP": {
         const tipToEdit = tips[action.payload];
@@ -86,18 +72,7 @@ function useData() {
           },
         };
       }
-      default:
-      case "NEW_TIP": {
-        return {
-          data: dataStarter,
-          metadata: {
-            ...oldMetadata,
-            inputPointer: -1,
-            editing: true,
-          },
-        };
-      }
-      case "CANCEL_EDIT": {
+      case "CANCEL_TIP": {
         return {
           data: dataStarter,
           metadata: {
@@ -106,6 +81,57 @@ function useData() {
             editing: false,
           },
         };
+      }
+      case "SAVE_TIP": {
+        console.log(dispatchTips, formattedDate(), nextTipId);
+        return;
+      }
+      case "PREVIEW_TIP": {
+        console.log(dispatchTips);
+        return;
+      }
+      case "REPLACE_TITLE": {
+        oldData.title = action.payload.value;
+        return { data: oldData, metadata: oldMetadata };
+      }
+      case "TOGGLE_TAG": {
+        if (action.payload.active)
+          oldMetadata.existingTagsSet.add(action.payload.id);
+        if (!action.payload.active)
+          oldMetadata.existingTagsSet.delete(action.payload.id);
+        oldData.tags = [
+          ...oldMetadata.existingTagsSet,
+          ...oldMetadata.newTagsArray,
+        ];
+        console.log();
+        return { data: oldData, metadata: oldMetadata };
+      }
+      case "CLEAR_TAGS": {
+        oldMetadata.existingTagsSet.clear();
+        oldData.tags = [
+          ...oldMetadata.existingTagsSet,
+          ...oldMetadata.newTagsArray,
+        ];
+        return { data: oldData, metadata: oldMetadata };
+      }
+      case "UPDATE_NEW_TAGS": {
+        const newString = action.payload;
+        oldData.newTagsArray =
+          newString.length === 0 ? [] : newString.split(" ");
+
+        return { data: oldData, metadata: oldMetadata };
+      }
+      case "REPLACE_SECTION_DATA_FIELD": {
+        const index = action.payload.index;
+        const value = action.payload.value;
+        const field = action.payload.field;
+        oldData.sections[index][field] = value;
+        return { data: oldData, metadata: oldMetadata };
+      }
+      case "ADD_SECTION": {
+        oldMetadata.focusId = oldData.sections.length;
+        oldData.sections = [...oldData.sections, { type: "text", content: "" }];
+        return { data: oldData, metadata: oldMetadata };
       }
     }
   }
