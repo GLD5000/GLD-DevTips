35824528b791cf3b587eb1f7ecf6c78c44208568
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-20 22:43:07 +0000

    Added inputFormContext

--- src/components/header/AddTip/MainTitle.js
+++ src/components/header/AddTip/MainTitle.js
@@ -1,37 +1,43 @@
+import { useInputFormContext } from "../../../contexts/Providers/InputFormProvider";
+
+
 function defaultOnInput(value, index) {
   console.log(`No function assigned!
   Value is: ${value}
   index/name is: ${index}`);
 }
 
-const MainTitle = ({
+export default function MainTitle ({
   placeholder = "Type here...",
   onInput = defaultOnInput,
   type = "text",
   name = undefined,
   defaultValue = "",
-}) => {
+}) {
+  const {
+    inputForm: { data: {title} },
+    dispatchInputForm,
+  } = useInputFormContext();
+
 
 
   const handler = (e) => {
     const value = e.target.value;
     const index = e.target.name;
-    onInput(value, index);
+    dispatchInputForm({type: "REPLACE_TITLE", value: value});
   };
 
 
   return (
     <input
-      // autoFocus
       className="titleInput"
       type="text"
       placeholder=" Add a title or topic for your tip..."
       onChange={(e) => handler(e)}
       name={name}
       autoComplete="off"
-      defaultValue={defaultValue}
+      defaultValue={title}
     ></input>
   );
 };
 
-export default MainTitle;
