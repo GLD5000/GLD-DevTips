527271f6d5d5b3fc5fa2ae8a0a02f8a98d0887ad
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-20 00:07:20 +0000

    Updated edit button

---
+++ src/components/tips/EditButton.js
@@ -0,0 +1,19 @@
+import SvgButton from "../../elements/SvgButton";
+import { useInputFormContext } from "../../contexts/Providers/InputFormProvider"
+export default function EditButton({tipId}) {
+    const {dispatchInputForm} = useInputFormContext();
+    function editTip(e){
+        dispatchInputForm({type: "EDIT_TIP", payload: e.target.id});
+    }
+  return (
+<SvgButton
+        wide="false"
+        type="write"
+        key={"edit"+tipId}
+        id={tipId}
+        buttonClasses="text-whitesmoke bg-transparent ml-auto"
+        text="Edit"
+        clickFunction={editTip}
+        svgClasses="stroke-whitesmoke stroke-1 fill-none"
+      />  )
+}
--- src/components/tips/Tip.js
+++ src/components/tips/Tip.js
@@ -1,37 +1,34 @@
 import MultiTextBoxes from "./MultiTextBoxes";
 import TipTitle from "../../elements/TipTitle";
+import EditButton from "./EditButton";
 import Tags from "./Tags";
 import { useState } from "react";
-import SvgButton from "../../elements/SvgButton";
 
-const Tip = ({ tip, setTagStateFromTip, editTip, showAddTipForm }) => {
+export default function Tip({
+  tip,
+  setTagStateFromTip,
+  editTip,
+  showAddTipForm,
+}) {
   const [expanded, setExpanded] = useState(() => false);
-  function toggleExpanded(){
+  function toggleExpanded() {
     setExpanded(!expanded);
   }
-  const title = tip.titleSuffix === undefined? tip.title: tip.title + tip.titleSuffix;
-  return ( 
-
-        <div className="tip">
-        <TipTitle title={title} onClick={toggleExpanded} expanded={expanded}/>
-        {expanded && <>
-        {showAddTipForm === false && <SvgButton
-        wide="false"
-        type="write"
-        key={tip.id}
-        id={tip.id}
-        buttonClasses="text-whitesmoke bg-transparent ml-auto"
-        text="Edit"
-        clickFunction={editTip}
-        svgClasses="stroke-whitesmoke stroke-1 fill-none"
-      />}
-        <MultiTextBoxes tip={tip} /> 
-        </>}
-        <Tags tagArray={tip.tags} setTagStateFromTip={setTagStateFromTip} />
-        {tip.updated &&  <p>{tip.updated}</p>}
-      </div>
-  
+  const title =
+    tip.titleSuffix === undefined ? tip.title : tip.title + tip.titleSuffix;
+  return (
+    <div className="tip">
+      <TipTitle title={title} onClick={toggleExpanded} expanded={expanded} />
+      {expanded && (
+        <>
+          {showAddTipForm === false && (
+            <EditButton key={"edit" + tip.id} tipId={tip.id} />
+          )}
+          <MultiTextBoxes tip={tip} />
+        </>
+      )}
+      <Tags tagArray={tip.tags} setTagStateFromTip={setTagStateFromTip} />
+      {tip.updated && <p>{tip.updated}</p>}
+    </div>
   );
-};
-
-export default Tip;
+}
