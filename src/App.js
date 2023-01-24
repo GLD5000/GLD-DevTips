c45dec35d36d88d2a536e350ac859417a178af03
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-24 18:25:01 +0000

    Removed dataprovider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import Header from "./components/header/Header";
 import Tips from "./components/tips/Tips";
 import TopSection from "./components/header/TopSection";
 import CombinedProviders from "./contexts/CombinedProviders";
-import DataProvider from "./DataProvider";
 
 export default function App() {
   return (
     <CombinedProviders>
-      <DataProvider>
         <section id="page-container" className=" h-screen flex flex-col items-start justify-items-center overflow-y-scroll text-zinc-100 border-zinc-600">
           <h1>Hello, my name is Gareth...</h1>
           <Header
@@ -19,7 +17,6 @@ export default function App() {
           </section>
           <section id="footer" className="footer"> footer</section>
         </section>
-      </DataProvider>
     </CombinedProviders>
   );
 }
