95a4cff0a0e519ac2380e1384296194fd8436271
GLD5000 <garethlouisdevlin@gmail.com>
2023-01-31 21:22:53 +0000

    Added aria-label to tip title buttons

--- src/elements/TipTitle.js
+++ src/elements/TipTitle.js
@@ -3,9 +3,11 @@ import ExpandSvg from '../components/icons/ExpandSvg';
 
 function TipTitle({ title, onClick, expanded }) {
   const conditionalClasses = expanded ? 'sticky top-16 py-4 z-50 ' : null;
+  const name = expanded ? `Collapse Tip` : `Expand Tip`;
   return (
     <button
       type="button"
+      aria-label={name}
       className={`${conditionalClasses} grid w-full grid-cols-frAutoFr justify-items-end rounded-none rounded-t-lg border-b border-b-neutral-600 bg-inherit hover:bg-slate-800 active:bg-slate-600`}
       onClick={onClick}
     >
