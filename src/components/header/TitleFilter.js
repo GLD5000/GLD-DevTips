6b1c7b02f9029009a56e3af6a9ffe9e5b4845291
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-01 11:58:26 +0000

    Removed unnecessary div

--- src/components/header/TitleFilter.js
+++ src/components/header/TitleFilter.js
@@ -3,11 +3,7 @@ import SearchTitle from '../../elements/SearchTitle';
 function TitleFilter({ toggleExpanded, expanded }) {
   const listId = 'searchTitleList';
   const title = 'Search Title';
-  return (
-    <div className="grid grid-rows-2 gap-2">
-      <SearchTitle listId={listId} title={title} onClick={toggleExpanded} expanded={expanded} />
-    </div>
-  );
+  return <SearchTitle listId={listId} title={title} onClick={toggleExpanded} expanded={expanded} />;
 }
 
 export default TitleFilter;
