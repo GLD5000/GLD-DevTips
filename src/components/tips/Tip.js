90c56e22ae42308d55ac453e83ce3ba3d490280b
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-01 11:36:42 +0000

    Added effect to expand tip only one is present

--- src/components/tips/Tip.js
+++ src/components/tips/Tip.js
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MultiTextBoxes from './MultiTextBoxes';
 import TipTitle from '../../elements/TipTitle';
 import EditButton from './EditButton';
 import Tags from './Tags';
 
-export default function Tip({ tip, updated }) {
-  const [expanded, setExpanded] = useState(() => false);
+export default function Tip({ tip, updated, shouldExpand }) {
+  const [expanded, setExpanded] = useState(shouldExpand);
   function handleClick() {
     setExpanded(!expanded);
   }
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted && shouldExpand) {
+      setExpanded(true);
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [shouldExpand]);
   const title = tip.titleSuffix === undefined ? tip.title : tip.title + tip.titleSuffix;
   return (
     <article className="flex  w-body min-w-body max-w-body flex-col items-start justify-between gap-2 rounded border bg-neutral-800 p-2">
--- src/components/tips/Tips.js
+++ src/components/tips/Tips.js
@@ -2,25 +2,19 @@ import Tip from './Tip';
 import { useFilteredTipsContext } from '../../contexts/Providers/FilteredTipsProvider';
 import { useInputFormContext } from '../../contexts/Providers/InputFormProvider';
 
-function Tips({ setTagStateFromTip, editTip, showAddTipForm }) {
+function Tips() {
   const tips = useFilteredTipsContext();
   const {
     inputForm: {
       metadata: { editing },
     },
   } = useInputFormContext();
+  const shouldExpand = tips.length === 1;
   return (
     <section id="tip-container" className="grid w-full grid-cols-1 justify-items-center gap-2">
       {!editing &&
         tips.map((tip) => (
-          <Tip
-            key={tip.id}
-            tip={tip}
-            setTagStateFromTip={setTagStateFromTip}
-            editTip={editTip}
-            showAddTipForm={showAddTipForm}
-            updated={tip.updated}
-          />
+          <Tip key={tip.id} tip={tip} updated={tip.updated} shouldExpand={shouldExpand} />
         ))}
     </section>
   );
