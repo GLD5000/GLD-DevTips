3abd0246de18b2363950e897e2a05f49765f7c48
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-02 21:01:00 +0000

    Updated height and padding

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,13 +12,13 @@ export default function Header({ title }) {
   } = useInputFormContext();
   const conditionalButton = editing ? <SaveButton /> : <AuthButton />;
   return (
-    <header className="sticky top-0 left-0 right-0 z-[999] grid h-16 w-screen grid-cols-frAutoFr content-center bg-neutral-800">
+    <header className="sticky top-0 left-0 right-0 z-[999] grid h-fit w-screen grid-cols-frAutoFr content-center bg-neutral-800">
       <nav className=" col-start-2 flex w-body min-w-body max-w-body flex-wrap items-center justify-between align-middle  ">
-        <div className="flex flex-wrap items-center gap-4 py-4">
+        <div className="flex h-16 flex-wrap items-center gap-4 p-2">
           <GldSvg />
           <h1>{title}</h1>
         </div>
-        <div className="relative flex h-full flex-wrap items-center justify-center gap-4 py-4">
+        <div className="relative flex h-16 flex-wrap items-center justify-center gap-4 py-2">
           <CreateButton />
           {conditionalButton}
         </div>
