67482e3a2b65ba6947dc030a4910787361436649
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-30 16:13:36 +0100

    Updated code box width etc.

--- src/elements/CodeBox.js
+++ src/elements/CodeBox.js
@@ -30,13 +30,15 @@ export default function CodeBox({ content, parse = false }) {
       : null;
   const codeBody = codeFileName ? code.slice(firstLineBreak + 1) : code;
   return (
-    <code className="font-mono my-2 mx-4 block h-fit max-w-full overflow-x-auto whitespace-pre rounded border-solid border-neutral-500 bg-neutral-900 text-vsGreen placeholder:text-vsGreen">
-      {codeFileName && (
-        <h3 className="file-name rounded-none bg-neutral-900 px-1 text-right text-neutral-300">
-          {codeFileName}
-        </h3>
-      )}
-      <pre className="p-2">{`${codeBody}`}</pre>
+    <code className="font-mono relative my-2 mx-auto block h-fit w-fit max-w-full overflow-x-auto whitespace-pre rounded border-solid border-neutral-500 bg-black text-vsGreen placeholder:text-vsGreen">
+      <div className="p-2">
+        {codeFileName && (
+          <h3 className="file-name rounded-none bg-black px-1 text-right text-neutral-300">
+            {codeFileName}
+          </h3>
+        )}
+        <pre className="p-2">{`${codeBody}`}</pre>
+      </div>
       <SvgButton
         type="duplicate"
         key={`${1}copyCode`}
@@ -49,9 +51,9 @@ export default function CodeBox({ content, parse = false }) {
         showText
         reverse={false}
         buttonClasses="border-transparent sticky left-0"
-        className=" sticky left-0 bottom-0 flex w-full items-center  justify-center rounded-none border border-transparent bg-neutral-900 text-neutral-200 hover:border hover:border-current hover:transition focus:transition active:bg-slate-600"
+        className=" sticky left-0 bottom-0 flex w-full items-center  justify-center rounded-none border border-transparent bg-black text-neutral-200 hover:border hover:border-current hover:transition focus:transition active:bg-slate-600"
         // marginLeft='auto'
-        svgClasses="stroke-1 fill-neutral-900 stroke-neutral-200"
+        svgClasses="stroke-1 fill-black stroke-neutral-200"
       />
     </code>
   );
