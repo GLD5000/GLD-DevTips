690970d2fd2b686f0186152d55fe167aef6dfc79
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-12 15:35:23 +0100

    Updated copy icon

--- src/components/icons/DuplicateSvg.js
+++ src/components/icons/DuplicateSvg.js
@@ -2,21 +2,10 @@ export default function DuplicateSvg() {
   return (
     <div className="pointer-events-none m-1 h-6 w-6">
       <svg id="duplicate-svg" alt="copy" height="100%" width="100%" viewBox="0 0 16 16">
-        <rect
-          x="2"
-          y="2"
-          width="9"
-          height="9"
-          rx="1"
-          className="fill-neutral-100 stroke-current stroke-1 dark:fill-neutral-800"
-        />
-        <rect
-          x="5"
-          y="5"
-          width="9"
-          height="9"
-          rx="1"
-          className="fill-neutral-100 stroke-current stroke-1 dark:fill-neutral-800"
+        <path
+          className="stroke-1, fill-none stroke-current"
+          style={{ strokeLinejoin: 'round' }}
+          d="M 1.5,1.5 V 11.5 h 3 V 4.5 H 11.5 v -3 z m 3,10 v 3 H 14.5 V 4.5 h -3"
         />
       </svg>
     </div>
